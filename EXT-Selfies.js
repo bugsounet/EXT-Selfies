88fa1bc9aa7ebf9b8009d14eff3780333c67950b
bugsounet <bugsounet@bugsounet.fr>
2022-11-02 18:02:49 +0100

    some correct ! good job ;)

--- EXT-Selfies.js
+++ EXT-Selfies.js
@@ -4,6 +4,15 @@
 *  10/2022          *
 ********************/
 
+/** Warn: use `npm run update` for updating **/
+
+/** @todo:
+ * move backgrounds from `showLastPhoto: {}` to `prepare: {}`
+ * decrease main background (master.png) size ?
+**/
+
+/** good way ! **/
+
 Module.register("EXT-Selfies", {
   defaults: {
     debug: false,
@@ -14,7 +23,7 @@ Module.register("EXT-Selfies", {
     shootMessage: "Smile!",
     shootCountdown: 5,
     displayButton: true,
-    buttonStyle: "1", // Set "1", "2", "3", "4", "5" ------> or ["1", "3,"5"]
+    buttonStyle: 1, // Set 1, 2, 3, 4, 5 --- // don't try to past a string as a number ;)
     updateInterval: 7 * 1000, // *
     animationSpeed: 3000,
     playShutter: true,
@@ -78,7 +87,6 @@ Module.register("EXT-Selfies", {
   },
 
   start: function() {
-   /** initialize all values before using **/
     /** initialize all values before using **/
     this.session = {}
     this.sendSocketNotification("INIT", this.config)
@@ -88,32 +96,29 @@ Module.register("EXT-Selfies", {
     this.logoRetry = "/modules/EXT-Selfiesresources/retry.png"
     this.buttonUrls = {
       1: "/modules/EXT-Selfies/resources/master.png",
-      2: "/modules/EXT-Selfies/resources/halloween.png",
-      3: "/modules/EXT-Selfies/resources/birthday.png",
-      4: "/modules/EXT-Selfies/resources/christmas.png"
+      2: "/modules/EXT-Selfies/resources/halloween.png", // not defined
+      3: "/modules/EXT-Selfies/resources/birthday.png",  // not defined
+      4: "/modules/EXT-Selfies/resources/christmas.png"  // not defined
     }
-    this.logoSelfies = this.buttonUrls[1]
+
     /** end of initialize **/
-    if (this.config.buttonStyle) { // @todo check this.buttonUrls length
+    if (this.config.buttonStyle && this.buttonUrls[this.config.buttonStyle]) {
       this.logoSelfies = this.buttonUrls[this.config.buttonStyle]
-    }
-    /* not needed use flash css
-    setInterval(() => {
-      this.updateDom(this.config.animationSpeed || 0); // use config.animationSpeed or revert to zero @ninjabreadman
-    }, this.config.updateInterval)
-    */
+    } else this.logoSelfies = this.buttonUrls[1] // <-- set default
   },
 
   getDom: function() {
     var wrapper = document.createElement("div")
+    var session = {}
     if (this.config.displayButton) {
-      var getTimeStamp = new Date()
       var icon = document.createElement("div")
 			icon.id = "EXT-SELFIES-BUTTON"
-			icon.style.backgroundImage = "url(" + this.logoSelfies + ")"
+      console.log(this.logoSelfies)
+			// icon.style.backgroundImage = "url(" + this.logoSelfies + ")"
+      icon.style.backgroundImage = `url(${this.logoSelfies})` // <-- better with this
 			icon.classList.add("flash")
-			var session = {}
 			icon.addEventListener("click", () => this.shoot(this.config, session))
+      wrapper.appendChild(icon) // <--- missing
     } else wrapper.style.display = 'none'
     return wrapper
   },
@@ -144,7 +149,7 @@ Module.register("EXT-Selfies", {
     dom.appendChild(shutter)
 
     var validateIcon = document.createElement("div")
-    validateIcon.id = "EXT-SELFIESS-VALIDATE"
+    validateIcon.id = "EXT-SELFIES-VALIDATE"
     validateIcon.classList.add("hidden")
 
     dom.appendChild(validateIcon)
@@ -156,7 +161,7 @@ Module.register("EXT-Selfies", {
     dom.appendChild(retryIcon)
 
     var exitIcon = document.createElement("div")
-    exitIcon.id = "EXT-SELFIESS-EXIT"
+    exitIcon.id = "EXT-SELFIES-EXIT"
     exitIcon.classList.add("hidden")
 
     dom.appendChild(exitIcon)
@@ -273,13 +278,14 @@ Module.register("EXT-Selfies", {
     var validateIcon = document.querySelector("#EXT-SELFIES-VALIDATE")
     validateIcon.classList.remove("hidden")
     validateIcon.style.backgroundImage = "url(" + this.logoValidate + ")"
-    icon.onclick = (event)=> {
-      this.sendNotification("EXT_SELFIES-RESULT",(result))
+    icon.onclick = (event)=> { // <--- are you sure it's icon?
+      this.sendNotification("EXT_SELFIES-RESULT", result)
     }
     var retryIcon = document.querySelector("#EXT-SELFIES-EXIT")
     retryIcon.classList.remove("hidden")
     retryIcon.style.backgroundImage = "url(" + this.logoRetry + ")"
-    icon.onclick = (event)=> {
+    icon.onclick = (event)=> { // <--- are you sure it's icon?
+      // warn: don't forget to reset session: {}
       this.sendNotification("EXT_SELFIES-SHOOT")
     }
     var exitIcon = document.querySelector("#EXT-SELFIES-VALIDATE")
